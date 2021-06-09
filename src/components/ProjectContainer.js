aafb674c608c897a03eaf2721cbbd87dee531154
Cyril Deschamps <77438408+CyrilDlaPompote@users.noreply.github.com>
2021-06-09 17:03:39 +0200

    forme projet

--- src/components/ProjectContainer.js
+++ src/components/ProjectContainer.js
@@ -1,3 +1,11 @@
-const ProjectContainer = () => {
-  
+const ProjectContainer = ({ src }) => {
+  return(
+    <div className="projectContainer">
+      <img className="image" src={src} alt="Projet" />
+      <p className="title">EIJZEOJZEOIR</p>
+      <p className="paragraphe">EIJZEOJZESQQSDQSDQSDQSDOIR</p>
+    </div>
+  )
 }
+
+export default ProjectContainer;
